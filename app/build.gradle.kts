f9511e65acf9a43405db8f42cb137f261984c7a1
BlindMint <blindmint@protonmail.com>
2025-10-09 18:59:43 -0600

    Update icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         minSdk = 26
         targetSdk = 35
 
-        versionCode = 14
-        versionName = "1.6.5"
+        versionCode = 15
+        versionName = "1.6.6"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
