fcd3ea84c699de380239fb93be95c99155656646
BlindMint <blindmint@protonmail.com>
2026-01-04 14:50:13 -0700

    Release v1.13.0
    - Bump versionCode to 24
    - Bump versionName to 1.13.0
    - Update README version badges

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         minSdk = 26
         targetSdk = 35
 
-        versionCode = 23
-        versionName = "1.12.0"
+        versionCode = 24
+        versionName = "1.13.0"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
