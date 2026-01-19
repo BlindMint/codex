726832757aecb5f3dcd40089560921a56f5695e4
BlindMint <blindmint@example.com>
2026-01-18 18:36:04 -0700

    Release v2.2.1
    - Bump versionCode to 32
    - Bump versionName to 2.2.1
    - Update README version badges

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         minSdk = 26
         targetSdk = 35
 
-        versionCode = 31
-        versionName = "2.2.0"
+        versionCode = 32
+        versionName = "2.2.1"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
